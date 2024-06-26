9ed3a2edd4e091df78200712843b3ac0ebe6c1af
Cao Yang <yang.cao@hytechc.com>
2024-06-26 21:09:32 +0800

    2024-06-26 done

---
+++ Leet/Problems/P2571.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Problems;
+
+/// <summary>
+/// 2571. Minimum Operations to Reduce an Integer to 0
+/// </summary>
+public class P2571
+{
+    public int MinOperations(int n)
+    {
+        // base case
+        if (IsPowerOf2(n)) return 1;
+
+        int prev = FindPreviousPowerOf2(n);
+        int next = FindNextPowerOf2(n);
+
+        if (Math.Pow(2, next) - n < n - Math.Pow(2, prev))
+        {
+            return 1 + MinOperations((int)Math.Pow(2, next) - n);
+        }
+        else
+        {
+            return 1 + MinOperations(n - (int)Math.Pow(2, prev));
+        }
+    }
+
+    public bool IsPowerOf2(int n)
+    {
+        int log = (int)Math.Log2(n);
+
+        return (int)Math.Pow(2, log) == n;
+    }
+
+    public int FindPreviousPowerOf2(int n)
+    {
+        return (int)Math.Log2(n);
+    }
+
+    public int FindNextPowerOf2(int n)
+    {
+        return (int)(Math.Log2(n) + 1);
+    }
+}
---
+++ Leet/Problems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problems;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+
+    /// <summary>
+    /// 2024-06-26
+    /// </summary>
+    public static void RunP2571()
+    {
+        P2571 p2571 = new P2571();
+        int num = p2571.MinOperations(33);
+        Console.WriteLine(num);
+    }
+}
