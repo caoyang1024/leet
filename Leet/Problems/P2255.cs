6c19dbf13ce5d1144d3486ac1edff2cdd20ecf92
Cao Yang <yang.cao@hytechc.com>
2024-06-27 21:23:34 +0800

    add p2255

---
+++ Leet/Problems/P2255.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Problems;
+
+/// <summary>
+/// 2255. Count Prefixes of a Given String
+/// </summary>
+public class P2255
+{
+    public int CountPrefixes(string[] words, string s)
+    {
+        int count = 0;
+
+        char[] charArr = s.ToCharArray();
+
+        foreach (var word in words)
+        {
+            var wordArr = word.ToCharArray();
+
+            if (wordArr.Length > charArr.Length)
+            {
+                continue;
+            }
+
+            bool isPrefix = true;
+
+            for (int i = 0; i < Math.Min(charArr.Length, wordArr.Length); i++)
+            {
+                if (charArr[i] != wordArr[i])
+                {
+                    isPrefix = false;
+                    break;
+                }
+            }
+
+            if (isPrefix)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
