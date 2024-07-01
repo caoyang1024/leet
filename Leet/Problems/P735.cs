9a6244f9114869ac6fd95023d8a7d9d0a37b1251
Cao Yang <yang.cao@hytechc.com>
2024-07-01 23:30:01 +0800

    add P735

---
+++ Leet/Problems/P735.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+/// <summary>
+/// 735. Asteroid Collision
+/// </summary>
+public class P735
+{
+    public int[] AsteroidCollision(int[] asteroids)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        foreach (var asteroid in asteroids)
+        {
+            if (stack.Count > 0 && stack.Peek() > 0 && asteroid < 0)
+            {
+                while (true)
+                {
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(asteroid);
+                        break;
+                    }
+
+                    if (SameDirection(stack.Peek(), asteroid))
+                    {
+                        stack.Push(asteroid);
+                        break;
+                    }
+                    if (stack.Peek() > Math.Abs(asteroid))
+                    {
+                        break;
+                    }
+
+                    if (stack.Peek() < Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                    }
+                    else if (stack.Peek() == Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                        break;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+            else
+            {
+                stack.Push(asteroid);
+            }
+        }
+
+        int[] arr = new int[stack.Count];
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            arr[i] = stack.Pop();
+        }
+
+        bool SameDirection(int x, int y)
+        {
+            return (x > 0 && y > 0) || (x < 0 && y < 0);
+        }
+
+        return arr;
+    }
+}
--- Leet/Problems/Program.cs
+++ Leet/Problems/Program.cs
@@ -6,8 +6,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        P2216 p2216 = new P2216();
-        int num = p2216.MinDeletion(new[] { 2, 6, 2, 5, 8, 9, 7, 2, 2, 5, 6, 2, 2, 0, 6, 8, 7, 3, 9, 2, 1, 1, 3, 2, 6, 2, 4, 6, 5, 8, 4, 8, 7, 0, 4, 8, 7, 8, 4, 1, 1, 4, 0, 1, 5, 7, 7, 5, 9, 7, 5, 5, 8, 6, 4, 3, 6, 5, 1, 6, 7, 6, 9, 9, 6, 8, 6, 0, 9, 5, 6, 7, 6, 9, 5, 5, 7, 3, 0, 0, 5, 5, 4, 8, 3, 9, 3, 4, 1, 7, 9, 3, 1, 8, 8, 9, 1, 6, 0, 0 });
-        Console.WriteLine(num);
+        P735 p735 = new P735();
+        int[] arr = p735.AsteroidCollision([1, -2, -2, -2]);
+
+        foreach (var i in arr)
+        {
+            Console.Write(i + ", ");
+        }
     }
 }
