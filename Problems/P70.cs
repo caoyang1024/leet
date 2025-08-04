16d5fd0a820d87ebe8acd7439bbcaec03d0e38ef
Cao Yang <yang.cao@hytechc.com>
2025-08-04 16:55:26 +0800

    add 70 Climb Stairs

---
+++ Problems/P70.cs
@@ -0,0 +1,21 @@
+﻿namespace Problems;
+
+public class P70
+{
+    public int ClimbStairs(int n)
+    {
+        if (n <= 2) return n;
+
+        int[] ways = new int[n + 1];
+
+        ways[1] = 1;
+        ways[2] = 2;
+
+        for (int i = 3; i <= n; i++)
+        {
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+
+        return ways[n];
+    }
+}
