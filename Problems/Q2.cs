b84934025cfd0e74225e6e74b4417f21d31029d5
Cao Yang <yang.cao@hytechc.com>
2025-08-06 18:03:08 +0800

    Q2 wip

---
+++ Problems/Q2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class Q2
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        Stack<int> s1 = new Stack<int>();
+        Stack<int> s2 = new Stack<int>();
+
+        while (l1 != null)
+        {
+            s1.Push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null)
+        {
+            s2.Push(l2.val);
+            l2 = l2.next;
+        }
+
+        ListNode node = new ListNode();
+        int remaining = 0;
+        while (s1.Count > 0 || s2.Count > 0)
+        {
+            ListNode currentNode = new ListNode();
+
+            int top1 = s1.Count > 0 ? s1.Pop() : 0;
+            int top2 = s2.Count > 0 ? s2.Pop() : 0;
+
+            if (top1 + top2 + remaining >= 10)
+            {
+                currentNode.val = (top1 + top2 + remaining) % 10;
+                remaining = 1;
+            }
+            else
+            {
+                currentNode.val = top1 + top2;
+                remaining = 0;
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
