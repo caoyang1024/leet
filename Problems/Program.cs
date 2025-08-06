5b634b910991fb252475552170bb5aaa9896cbd4
Cao Yang <yang.cao@hytechc.com>
2025-08-06 20:37:14 +0800

    add 3sum

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,13 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problems;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public IList<IList<int>> ThreeSum(int[] nums)
     {
-        P300 p = new();
-        Console.WriteLine(p.LengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
+        Array.Sort(nums);
 
+        List<IList<int>> list = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                return list;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            var twoSumList = TwoSum(nums, i + 1, nums.Length - 1, -nums[i]);
+
+            foreach (var pair in twoSumList)
+            {
+                list.Add([nums[i], pair.Item1, pair.Item2]);
+            }
+        }
+
+        return list;
+    }
+
+    private List<(int, int)> TwoSum(int[] nums, int i, int j, int target)
+    {
+        HashSet<(int, int)> twoSum = new HashSet<(int, int)>();
+
+        while (i < j)
+        {
+            if (nums[i] + nums[j] > target)
+            {
+                j--;
+            }
+            else if (nums[i] + nums[j] < target)
+            {
+                i++;
+            }
+            else
+            {
+                twoSum.Add((nums[i], nums[j]));
+                i++;
+                j--;
+            }
+        }
+
+        return twoSum.ToList();
     }
 }
---
+++ Problems/Q15.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class Q15
+{
+    public IList<IList<int>> ThreeSum(int[] numbers)
+    {
+        Array.Sort(numbers);
+
+        var list = new List<IList<int>>();
+
+        HashSet<(int, int)> twoSum = new HashSet<(int, int)>();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > 0)
+            {
+                return list;
+            }
+
+            if (i > 0 && numbers[i] == numbers[i - 1])
+            {
+                continue;
+            }
+
+            var twoSumList = TwoSum(i + 1, numbers.Length - 1, -numbers[i]);
+
+            foreach (var pair in twoSumList)
+            {
+                list.Add([numbers[i], pair.Item1, pair.Item2]);
+            }
+        }
+
+        return list;
+
+        IEnumerable<(int, int)> TwoSum(int i, int j, int target)
+        {
+            twoSum.Clear();
+
+            while (i < j)
+            {
+                if (numbers[i] + numbers[j] > target)
+                {
+                    j--;
+                }
+                else if (numbers[i] + numbers[j] < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    if (twoSum.Add((numbers[i], numbers[j])))
+                    {
+                        yield return (numbers[i], numbers[j]);
+                    }
+
+                    i++;
+                    j--;
+                }
+            }
+        }
+    }
+}
