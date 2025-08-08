ac29972742d7609a2f6f9a22c676daa04f4f848f
Cao Yang <yang.cao@hytechc.com>
2025-08-08 19:30:45 +0800

    add 128 LongestConsecutive

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -9,11 +9,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Q347 q = new();
+        Q128 q = new();
 
         TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
 
-        var res = q.TopKFrequent([1, 1, 1, 2, 2, 3], 2);
+        var res = q.LongestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]);
 
         Console.WriteLine(res);
         Console.WriteLine(JsonSerializer.Serialize(res));
---
+++ Problems/Q128.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class Q128
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        int longest = 0;
+
+        Dictionary<int, bool> mem = [];
+
+        foreach (int num in nums)
+        {
+            mem[num] = false;
+        }
+
+        foreach (int num in nums)
+        {
+            if (mem[num] == false)
+            {
+                int leftCount = SearchLeft(num - 1);
+                int rightCount = SearchRight(num + 1);
+
+                mem[num] = true;
+
+                longest = Math.Max(longest, leftCount + rightCount + 1);
+            }
+        }
+
+        return longest;
+
+        int SearchLeft(int n)
+        {
+            int total = 0;
+
+            while (mem.ContainsKey(n))
+            {
+                mem[n] = true;
+                total++;
+                n--;
+            }
+
+            return total;
+        }
+
+        int SearchRight(int n)
+        {
+            int total = 0;
+
+            while (mem.ContainsKey(n))
+            {
+                mem[n] = true;
+                total++;
+                n++;
+            }
+
+            return total;
+        }
+    }
+}
