9e27db1176dded3a90598ab2bcceeae34266d5f5
Cao Yang <yang.cao@hytechc.com>
2025-08-14 20:53:08 +0800

    wip

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using Problems.ADT;
-using Problems.DP;
-
-namespace Problems;
+﻿namespace Problems;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Q45 q = new();
-
-        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
-
-        Console.WriteLine(q.Jump([2, 1]));
-        //Console.WriteLine(q.Jump([1]));
-        //Console.WriteLine(q.Jump([2, 3, 1, 1, 4]));
-        //Console.WriteLine(q.Jump([3, 2, 1, 0, 4]));
     }
 }
